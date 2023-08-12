bf90c360ca14b0b9f8aa4ac44c2a536b7408c885
Arpit Chavan <arpit@example.com>
2023-08-12 15:55:33 +0530

    trial 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,6 +9,7 @@ public class Main {
     }
 
     public static class B extends A {
+
         @Override
         public String meth() {
             return "Method is overridden in Extended class B";
@@ -18,11 +19,11 @@ public class Main {
         // Task 3
         B objB = new B();
         String resultTask3 = objB.meth();
-        System.out.println(resultTask3);  // This will print: "Invoking method from class A"
+        //System.out.println(resultTask3);  // This will print: "Invoking method from class A"
 
         // Task 5
         B objBOverride = new B();
         String resultTask5 = objBOverride.meth();
-        System.out.println(resultTask5);  // This will print: "Method is overridden in Extended class B"
+        //System.out.println(resultTask5);  // This will print: "Method is overridden in Extended class B"
     }
 }
