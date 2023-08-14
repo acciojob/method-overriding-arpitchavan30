f9191f3f69e2c5698385bc68ec4b5b8e508c3cef
Arpit Chavan <arpit@example.com>
2023-08-14 23:00:53 +0530

    version4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,52 +1,52 @@
 package com.driver;
 
-
-public class Main {
-    public static class A {
-        public String meth() {
-            return "Invoking method from class A";
-        }
-    }
-    public static class B extends A {
-
-
-        public String meth() {
-            System.out.println(super.meth());
-            return "Method is overridden in Extended class B";
-        }
-    }
-    public static void main(String args[]) {
-       B b1=new B();
-       b1.meth();
-
-       B b2=new B();
-       b2.meth();
-
-    }
-}
-
-
-
-//public class Main
-//{
-//    public static void main(String args[]){
-//        B b1=new B();
-//        b1.meth();
 //
-//        B b2=new B();
-//        b2.meth();
-//    }
-//    public static class A
-//    {
-//        public String meth ()
-//        {
+//public class Main {
+//    public static class A {
+//        public String meth() {
 //            return "Invoking method from class A";
 //        }
 //    }
 //    public static class B extends A {
-//        public String meth(){
+//
+//
+//        public String meth() {
 //            System.out.println(super.meth());
-//            return "Method is overridden in Extendend class B";
+//            return "Method is overridden in Extended class B";
 //        }
 //    }
+//    public static void main(String args[]) {
+//       B b1=new B();
+//       b1.meth();
+//
+//       B b2=new B();
+//       b2.meth();
+//
+//    }
 //}
+
+
+
+public class Main
+{
+    public static void main(String args[]){
+        B b1=new B();
+        b1.meth();
+
+        B b2=new B();
+        b2.meth();
+    }
+    public static class A
+    {
+        public String meth ()
+        {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        public String meth(){
+            System.out.println(super.meth());
+            return "Method is overridden in Extendend class B";
+        }
+    }
+}
